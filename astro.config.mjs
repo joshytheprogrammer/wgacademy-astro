8d201ea0fea015a6c9a9e2fad5cbe6f92ea0fe24
joshytheprogrammer <jurassicjoshua@protonmail.com>
2024-08-06 12:33:09 +0100

    made changes: ''

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify";
+
+import sanity from "@sanity/astro";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
+  integrations: [
+    sanity({
+      projectId: 'jjs4hd8k',
+      dataset: 'production',
+      useCdn: false, // See note on using the CDN
+      apiVersion: "2024-08-6", // insert the current date to access the latest version of the API
+    }), 
+    react()]
 });
